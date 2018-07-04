cc4ea8a2ba7d8294434de2f081706bb8b4649782
DESKTOP-D9VPJH0\vanpa <vpattonjr@gmail.com>
2018-07-03 22:21:23 -0500

    PuzzlePlatform

---
+++ Source/PuzzlePlatforns/PuzzlePlatfornsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PuzzlePlatfornsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APuzzlePlatfornsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APuzzlePlatfornsGameMode();
+};
+
+
+
